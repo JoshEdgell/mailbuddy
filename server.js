b39cafcdff876da2b171e5148da3b30494f06a6e
JoshEdgell <josh.0.edgell@gmail.com>
2018-02-27 20:35:08 -0500

    added cors

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express               = require('express');
 const nodemailer            = require('nodemailer');
+const cors                  = require('cors');
 const app                   = express();
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Accept');
-  next()
-})
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Accept');
+//   next()
+// })
+app.use(cors());
 
 const smtpTransport = nodemailer.createTransport({
   service: 'gmail',
