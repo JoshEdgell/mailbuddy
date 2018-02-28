e94ab4f7e1763fe14e9f54818f67ed7ce1821372
JoshEdgell <josh.0.edgell@gmail.com>
2018-02-28 06:48:24 -0500

    added CrossDomain function

--- server.js
+++ server.js
@@ -3,14 +3,29 @@ const nodemailer            = require('nodemailer');
 const cors                  = require('cors');
 const app                   = express();
 
-app.use(cors());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+const allowCrossDomain = function(req,res,next){
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  } else {
     next();
-});
+  }
+}
+
+app.use(allowCrossDomain);
+// app.use(cors());
+
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", '*');
+//     res.header("Access-Control-Allow-Credentials", true);
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+//     next();
+// });
 
 const smtpTransport = nodemailer.createTransport({
   service: 'gmail',
@@ -21,7 +36,6 @@ const smtpTransport = nodemailer.createTransport({
   }
 })
 
-app.options('/send', cors());
 app.get('/send', cors(), (req,res)=>{
   var messageBody = 'This message was sent from an automatic mailer.  The message body begins below the line.\n================================\n\n' + req.query.text
   const mailOptions = {
