476a48acb97bb657bb6fe58417ec092f1c7549a1
JoshEdgell <josh.0.edgell@gmail.com>
2018-03-04 00:42:24 -0500

    updated package.json

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ $(()=>{
     $('#message').text('Sending E-mail...Please wait');
 
     // $.get('http://localhost:3000/send',{to:to,subject:subject,text:text},function(data){
-    $.get('https://cors-anywhere.herokuapp.com/easymailer.herokuapp.com/send',{to:to,subject:subject,text:text},function(data){
+    $.get('https://cors-anywhere.herokuapp.com/https://easymailer.herokuapp.com/send',{to:to,subject:subject,text:text},function(data){
       if(data=="sent")
       {
         $('#message').empty().html("Email has been sent to " + to + ". Please check inbox!");
