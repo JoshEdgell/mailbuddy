8638f1711d1b4044b73106a53517d07f5a0772b3
JoshEdgell <josh.0.edgell@gmail.com>
2018-03-04 00:45:30 -0500

    added cors

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const app                   = express();
 //   res.header("Access-Control-Allow-Credentials", true);
 //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 //   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-//
 //   if ('OPTIONS' == req.method) {
 //     res.send(200);
 //   } else {
@@ -18,7 +17,7 @@ const app                   = express();
 // }
 //
 // app.use(allowCrossDomain);
-// app.use(cors());
+app.use(cors());
 
 // app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", '*');
