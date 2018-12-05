3ef69df36ecc57e5e11b41e4c4473d0f91a0abc5
JoshEdgell <josh.0.edgell@gmail.com>
2018-12-04 19:55:55 -0500

    tried setting orign to whiteList

--- server.js
+++ server.js
@@ -8,13 +8,14 @@ const whiteList = [
   'http://mollyvyoung.com'
 ]
 const corsOptions = {
-  origin: function(origin, callback){
-    if (whiteList.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
+  // origin: function(origin, callback){
+  //   if (whiteList.indexOf(origin) !== -1) {
+  //     callback(null, true)
+  //   } else {
+  //     callback(new Error('Not allowed by CORS'))
+  //   }
+  // }
+  origin: whiteList
 }
 
 const transporter = nodemailer.createTransport({
