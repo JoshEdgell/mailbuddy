9b8f50d4db1b8d7d9629c1de8c17ae84b38b7959
JoshEdgell <josh.0.edgell@gmail.com>
2018-12-04 23:29:18 -0500

    let's try an array of origins

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app                   = express();
 // const whiteList = 'http://mollyvyoung.com'
 
 const corsOptions = {
-  origin: 'http://joshedgell.com',
+  origin: ['mollyvyoung.com', 'http://joshedgell.com'],
   optionsSuccessStatus: 200
 }
 
